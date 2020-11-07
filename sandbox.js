d579d09b20c1ba795ec6d2103d0cd903607860f1
Darlhem J <joseph.darlhem@gmail.com>
2020-11-07 17:41:47 -0600

    Learned about syntax parser and lexical environment

---
+++ sandbox.js
@@ -0,0 +1,24 @@
+/*Syntax Parcer: is a program that reads your code and determines what it does and if its
+* grammar is valid.
+*
+* parsing takes strings of character commands or a block of code and split them apart
+* and break it down into separate pieces or components which are scanned for correct
+* syntax or vocabulary  put it into a compiler and transforms it into machine language.
+*
+*
+* Lexical Environment is where something sits physically in the code you write
+* lexical means having to do with words or grammar so a lexical environment
+* exist in programming languages to which where you write something is important.
+*
+
+* */
+
+function hello() {
+    var a = " hello world ";
+}
+// the variable "a" sits lexical or where its physically sits inside the function
+// in the code above, before it is executed it pass through a syntax parser to machine
+// language and the computer stores that in memory and where you see things written
+// can let you have an idea of where it will fit in memory.
+//
+//
