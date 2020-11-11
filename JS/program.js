ecce76a88c5bdbceea691efa54ec271479a3cf29
Darlhem J <joseph.darlhem@gmail.com>
2020-11-11 16:10:22 -0600

    First commit for new files added

---
+++ JS/program.js
@@ -0,0 +1,17 @@
+console.log("test");
+
+document.writeln("Hello World");
+
+/*variable names are used for statements, variables, parameters,
+property names, operators, and labels.*/
+
+
+
+
+
+
+
+
+
+
+
