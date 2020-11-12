549b5658d8822c628ffc720aeba919af45df4760
Darlhem J <joseph.darlhem@gmail.com>
2020-11-11 19:03:44 -0600

    Added some comments on the object literal

--- JS/program.js
+++ JS/program.js
@@ -6,7 +6,7 @@ document.writeln("Hello World");
 property names, operators, and labels.*/
 
 /*THe (&&) or and operator produces the value of its first operand
-* if the first opeand is falsy. Otherwise, it produces the value of
+* if the first operand is falsy. Otherwise, it produces the value of
 * the second operand
 *
 * The (||) or (or) operator produces the value of its operand if
@@ -14,7 +14,7 @@ property names, operators, and labels.*/
 * second operand.*/
 
 // There are 6 object literals tha is known right now they as follow
-// number literal, string literal, object lilteral, array literal,
+// number literal, string literal, object literal, array literal,
 // function literal, and regexp literal.
 
 
@@ -26,13 +26,23 @@ const apartment = {
 	bath: "2",
 	address: {
 		street: "385 Main St.",
-		city: "Montvill",
+		city: "Dinar",
 		state: "Tx",
 		zip: "76358"
 	}
 };
 
-document.write(apartment.address.street);
+// Update the value iin an object can be updated by assignment, if the prop name exist.
+
+document.write(apartment.address.street = "124 Money St.");
+
+
+console.log(apartment.room = "5");
+console.log(apartment.address.city = "Austin");
+
+// if the object does not have that property name, that property is added to the
+// object.
+
 
 
 
