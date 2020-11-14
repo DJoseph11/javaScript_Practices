4369b99ed8c3b05a8d136e9ae055687937f1ba08
Darlhem J <joseph.darlhem@gmail.com>
2020-11-14 00:17:38 -0600

    Second commit

---
+++ JS/javaScriptParts.js
@@ -0,0 +1,23 @@
+// This js file is for me trying to understand javaScript, the fundamental aspect of working with the language.
+
+/*  syntax parsers --- A program that reads your code and determines what it does and if its grammar is valid
+*
+*   lexical environment--- Where something sits physically in the coe4e you write
+*
+*   Execution context--- a wrapper to help manage the code that is running*/
+
+
+/*  Name and value pair--- a name which maps to a unique value
+*           // address = "100 main st."
+*   Object--- A collection of name value pairs
+*           // a value in a name can store other name value pairs
+* */
+
+// Global environment and the global object
+
+/*  Whenever Js codes runs it runs inside an execution context meaning that the javascript engine wrap your code
+*   and do its thing,like parsing and such.
+*   And the base execution context is the global execution context.
+*
+*   the context create two things in the process it creates a " global object " and " this "
+*    */
